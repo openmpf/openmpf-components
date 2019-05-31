367ef538f0be0700b6cb6b326eb47fc40be7a6ca
jrobble <jrobble@mitre.org>
2019-05-31 17:03:34 -0400

    Tesseract API objects are now stored between GetDetections() calls.

--- cpp/TesseractOCRTextDetection/sample_tesseract_ocr_detector.cpp
+++ cpp/TesseractOCRTextDetection/sample_tesseract_ocr_detector.cpp
@@ -45,23 +45,23 @@ using namespace COMPONENT;
 
 using std::to_string;
 
-void print_usage(char* argv[]){
+void print_usage(char *argv[]) {
 
     std::cout << "Usage: " << argv[0] << " -i <IMAGE_DATA_URI> [TESSERACT_LANGUAGES]" << std::endl;
     std::cout << "Usage: " << argv[0] << " -g <GENERIC_DATA_URI> [TESSERACT_LANGUAGES]" << std::endl;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
-  if ((argc < 3)) {
+    if ((argc < 3)) {
         print_usage(argv);
         return 0;
     }
-  else{
-	std::string option(argv[1]);
-	std::string uri(argv[2]);
 
-	Properties algorithm_properties;
+    std::string option(argv[1]);
+    std::string uri(argv[2]);
+
+    Properties algorithm_properties;
     Properties media_properties;
     std::string job_name("OCR_test");
     algorithm_properties["TAGGING_FILE"] = "text-tags.json";
@@ -81,89 +81,105 @@ int main(int argc, char* argv[]) {
         // Run uri as a generic data file.
         std::cout << "Running job on generic data uri: " << uri << std::endl;
         MPFGenericJob job(job_name, uri, algorithm_properties, media_properties);
-        // Declare the vector of tracks to be filled in by the
-        // component.
+
+        // Declare the vector of tracks to be filled in by the component.
         std::vector<MPFGenericTrack> tracks;
+
         // Pass the job to the ocr component.
-        MPFDetectionError rc = MPF_DETECTION_SUCCESS;
-        rc = im.GetDetections(job, tracks);
-        if (rc == MPF_DETECTION_SUCCESS) {
+        if (MPF_DETECTION_SUCCESS == im.GetDetections(job, tracks)) {
             std::cout << "Number of tracks = "
                       << tracks.size() << std::endl;
             for (int i = 0; i < tracks.size(); i++) {
                 if (tracks[i].detection_properties.count("PRIMARY_SCRIPT") > 0) {
                     std::cout << "OSD result: " << "\n";
                     std::cout << "Detected script: " << tracks[i].detection_properties.at("PRIMARY_SCRIPT") << "\n";
-                    std::cout << "Script confidence: " << tracks[i].detection_properties.at("PRIMARY_SCRIPT_CONFIDENCE") << "\n";
-                    std::cout << "Script score: " << tracks[i].detection_properties.at("PRIMARY_SCRIPT_SCORE") << "\n";
+                    std::cout << "Script confidence: "
+                              << tracks[i].detection_properties.at("PRIMARY_SCRIPT_CONFIDENCE") << "\n";
+                    std::cout << "Script score: " << tracks[i].detection_properties.at("PRIMARY_SCRIPT_SCORE")
+                              << "\n";
                     std::cout << "Detected orientation: " << tracks[i].detection_properties.at("ROTATION") << "\n";
-                    std::cout << "Orientation confidence: " << tracks[i].detection_properties.at("ROTATION_CONFIDENCE") << "\n";
+                    std::cout << "Orientation confidence: "
+                              << tracks[i].detection_properties.at("ROTATION_CONFIDENCE") << "\n";
 
                     if (tracks[i].detection_properties.count("SECONDARY_SCRIPTS") > 0) {
-                        std::cout << "Secondary scripts: " << tracks[i].detection_properties.at("SECONDARY_SCRIPTS") << "\n";
-                        std::cout << "Secondary script scores: " << tracks[i].detection_properties.at("SECONDARY_SCRIPT_SCORES") << "\n"<< std::endl;
+                        std::cout << "Secondary scripts: " << tracks[i].detection_properties.at("SECONDARY_SCRIPTS")
+                                  << "\n";
+                        std::cout << "Secondary script scores: "
+                                  << tracks[i].detection_properties.at("SECONDARY_SCRIPT_SCORES") << "\n"
+                                  << std::endl;
                     }
                 }
                 if (tracks[i].detection_properties.count("TEXT") > 0) {
                     std::cout << "OCR result: " << i << "\n"
-                              << "   metadata = \"" << tracks[i].detection_properties.at("TEXT") << "\"" << std::endl;
+                              << "   metadata = \"" << tracks[i].detection_properties.at("TEXT") << "\""
+                              << std::endl;
                     std::cout << "OCR text tags: " << i << "\n"
-                              << "   detected string tags = \"" << tracks[i].detection_properties.at("TAGS") << "\"" << std::endl;
+                              << "   detected string tags = \"" << tracks[i].detection_properties.at("TAGS") << "\""
+                              << std::endl;
                     std::cout << "OCR language: " << tracks[i].detection_properties.at("TEXT_LANGUAGE") << "\n";
                     std::cout << "Confidence: " << tracks[i].confidence << "\n";
-                    std::cout << "OCR page_num: " << tracks[i].detection_properties.at("PAGE_NUM")  << "\n" << std::endl ;
+                    std::cout << "OCR page_num: " << tracks[i].detection_properties.at("PAGE_NUM") << "\n"
+                              << std::endl;
                 }
             }
         } else {
             std::cout << "GetDetections failed" << std::endl;
         }
+
     } else if (option == "-i") {
         // Run uri as an image data file.
         std::cout << "Running job on image data uri: " << uri << std::endl;
         MPFImageJob job(job_name, uri, algorithm_properties, media_properties);
-        // Declare the vector of image locations to be filled in by the
-        // component.
+
+        // Declare the vector of image locations to be filled in by the component.
         std::vector<MPFImageLocation> locations;
+
         // Pass the job to the ocr component.
-        MPFDetectionError rc = MPF_DETECTION_SUCCESS;
-        rc = im.GetDetections(job, locations);
-        if (rc == MPF_DETECTION_SUCCESS) {
+        if (MPF_DETECTION_SUCCESS == im.GetDetections(job, locations)) {
             std::cout << "Number of image locations = "
                       << locations.size() << std::endl;
 
             for (int i = 0; i < locations.size(); i++) {
                 if (locations[i].detection_properties.count("PRIMARY_SCRIPT") > 0) {
                     std::cout << "OSD result: " << "\n";
-                    std::cout << "Detected script: " << locations[i].detection_properties.at("PRIMARY_SCRIPT") << "\n";
-                    std::cout << "Script confidence: " << locations[i].detection_properties.at("PRIMARY_SCRIPT_CONFIDENCE") << "\n";
-                    std::cout << "Script score: " << locations[i].detection_properties.at("PRIMARY_SCRIPT_SCORE") << "\n";
-                    std::cout << "Detected orientation: " << locations[i].detection_properties.at("ROTATION") << "\n";
-                    std::cout << "Orientation confidence: " << locations[i].detection_properties.at("ROTATION_CONFIDENCE") << "\n";
+                    std::cout << "Detected script: " << locations[i].detection_properties.at("PRIMARY_SCRIPT")
+                              << "\n";
+                    std::cout << "Script confidence: "
+                              << locations[i].detection_properties.at("PRIMARY_SCRIPT_CONFIDENCE") << "\n";
+                    std::cout << "Script score: " << locations[i].detection_properties.at("PRIMARY_SCRIPT_SCORE")
+                              << "\n";
+                    std::cout << "Detected orientation: " << locations[i].detection_properties.at("ROTATION")
+                              << "\n";
+                    std::cout << "Orientation confidence: "
+                              << locations[i].detection_properties.at("ROTATION_CONFIDENCE") << "\n";
                     if (locations[i].detection_properties.count("SECONDARY_SCRIPTS") > 0) {
-                        std::cout << "Secondary scripts: " << locations[i].detection_properties.at("SECONDARY_SCRIPTS") << "\n";
-                        std::cout << "Secondary script scores: " << locations[i].detection_properties.at("SECONDARY_SCRIPT_SCORES") << "\n" << std::endl;
+                        std::cout << "Secondary scripts: "
+                                  << locations[i].detection_properties.at("SECONDARY_SCRIPTS") << "\n";
+                        std::cout << "Secondary script scores: "
+                                  << locations[i].detection_properties.at("SECONDARY_SCRIPT_SCORES") << "\n"
+                                  << std::endl;
                     }
 
                 }
                 if (locations[i].detection_properties.count("TEXT") > 0) {
                     std::cout << "OCR result: " << i << "\n"
-                              << "   metadata = \"" << locations[i].detection_properties.at("TEXT") << "\"" << std::endl;
+                              << "   metadata = \"" << locations[i].detection_properties.at("TEXT") << "\""
+                              << std::endl;
                     std::cout << "OCR language: " << locations[i].detection_properties.at("TEXT_LANGUAGE") << "\n";
                     std::cout << "Confidence: " << locations[i].confidence << "\n";
                     std::cout << "OCR text tags: " << i << "\n"
-                              << "   detected string tags = \"" << locations[i].detection_properties.at("TAGS") << "\""  << "\n" << std::endl ;
+                              << "   detected string tags = \"" << locations[i].detection_properties.at("TAGS")
+                              << "\"" << "\n" << std::endl;
                 }
             }
         } else {
-                std::cout << "GetDetections failed" << std::endl;
+            std::cout << "GetDetections failed" << std::endl;
         }
-     } else {
+
+    } else {
         print_usage(argv);
-        return 0;
-     }
+    }
 
-
-  }
-
+    im.Close();
     return 0;
 }
