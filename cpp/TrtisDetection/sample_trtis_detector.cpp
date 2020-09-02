cdfae0a5f35baf89b713734b787f412ed00f9577
jrobble <jrobble@mitre.org>
2020-09-01 23:00:16 -0400

    Fix sample binary.

--- cpp/TrtisDetection/sample_trtis_detector.cpp
+++ cpp/TrtisDetection/sample_trtis_detector.cpp
@@ -34,11 +34,11 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-    if ((argc < 2) || (argc > 5)) {
+    if ((argc < 2) || (argc > 4)) {
         cout << "argc = " << argc << endl;
-        cout << "Usage: " << argv[0] << " <image URI> <trtis_server> <num classifications> <confidence threshold>"
+        cout << "Usage: " << argv[0] << " <image URI> <trtis_server> <confidence threshold>"
              << endl;
-        return EXIT_SUCCESS;
+        return EXIT_FAILURE;
     }
 
     TrtisDetection trtis_component;
@@ -54,31 +54,38 @@ int main(int argc, char *argv[]) {
     // DEBUG
     algorithm_properties["TRTIS_SERVER"] = "localhost:8001";
     algorithm_properties["MODEL_NAME"] = "ip_irv2_coco";
-    algorithm_properties["CONFIDENCE_THRESHOLD"] = 0.2;
+    algorithm_properties["CONFIDENCE_THRESHOLD"] = "0.2";
     algorithm_properties["RESIZE"] = "true";
 
     string uri(argv[1]);
-    cout << "uri is " << uri << std::endl;
+    cout << "Media = " << uri << endl;
 
     if (argc > 2) { algorithm_properties["TRTIS_SERVER"] = string(argv[2]); }
-    cout << "Trtis server name and port = "
-         << algorithm_properties["TRTIS_SERVER"] << std::endl;
-
+    cout << "TRTIS server name and port = "
+         << algorithm_properties["TRTIS_SERVER"] << endl;
 
     if (argc > 3) { algorithm_properties["CONFIDENCE_THRESHOLD"] = string(argv[3]); }
     cout << "Confidence threshold = " << algorithm_properties["CONFIDENCE_THRESHOLD"] << endl;
 
     Properties media_properties;
-    string job_name("Testing Trtis");
+    string job_name("Testing TRTIS");
 
     MPFImageLocationVec detections;
     MPFImageJob job(job_name, uri, algorithm_properties, media_properties);
     detections = trtis_component.GetDetections(job);
+
+    cout << "Found " << detections.size() << " detections." << endl;
+
     for (int i = 0; i < detections.size(); i++) {
-        cout << "detection number " << i
-             << " classification is " << detections[i].detection_properties["CLASSIFICATION"]
-             << " and confidence is " << detections[i].confidence << endl;
+        string feature_type = detections[i].detection_properties["FEATURE TYPE"];
+        cout << "Detection number " << i << ":\n"
+             << "\tFEATURE TYPE = " << feature_type << endl;
+        if (feature_type == "CLASS") {
+            cout << "\tCLASSIFICATION = " << detections[i].detection_properties["CLASSIFICATION"] << endl;
+        }
+        cout << "\tConfidence = " << detections[i].confidence << endl;
     }
+
     trtis_component.Close();
     return EXIT_SUCCESS;
 }
