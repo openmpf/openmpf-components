e959a659fb5267a78cb5419f4b1b1144f5e31c5a
jrobble <jrobble@mitre.org>
2020-09-02 17:01:45 -0400

    Update sample binary.

--- cpp/TrtisDetection/sample_trtis_detector.cpp
+++ cpp/TrtisDetection/sample_trtis_detector.cpp
@@ -34,9 +34,10 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-    if ((argc < 2) || (argc > 4)) {
+    if ((argc < 2) || (argc > 5)) {
         cout << "argc = " << argc << endl;
-        cout << "Usage: " << argv[0] << " <image URI> <trtis_server> <confidence threshold>"
+        cout << "Usage: " << argv[0]
+             << " <image URI> <trtis-server> <class-confidence-threshold> <extra-confidence-threshold>"
              << endl;
         return EXIT_FAILURE;
     }
@@ -50,35 +51,49 @@ int main(int argc, char *argv[]) {
     }
 
     Properties algorithm_properties;
-
-    // DEBUG
     algorithm_properties["TRTIS_SERVER"] = "localhost:8001";
     algorithm_properties["MODEL_NAME"] = "ip_irv2_coco";
-    algorithm_properties["CONFIDENCE_THRESHOLD"] = "0.2";
-    algorithm_properties["RESIZE"] = "true";
+
+    algorithm_properties["FRAME_FEATURE_ENABLE"] = "true";
+
+    algorithm_properties["CLASS_FEATURE_ENABLE"] = "true";
+    algorithm_properties["CLASS_CONFIDENCE_THRESHOLD"] = "-1";
+
+    algorithm_properties["EXTRA_FEATURE_ENABLE"] = "true";
+    algorithm_properties["EXTRA_CONFIDENCE_THRESHOLD"] = "-1";
+
+    algorithm_properties["USER_FEATURE_ENABLE"] = "true";
+    algorithm_properties["USER_FEATURE_X_LEFT_UPPER"] = "0";
+    algorithm_properties["USER_FEATURE_Y_LEFT_UPPER"] = "0";
+    algorithm_properties["USER_FEATURE_WIDTH"] = "100";
+    algorithm_properties["USER_FEATURE_HEIGHT"] = "100";
 
     string uri(argv[1]);
     cout << "Media = " << uri << endl;
 
     if (argc > 2) { algorithm_properties["TRTIS_SERVER"] = string(argv[2]); }
-    cout << "TRTIS server name and port = "
-         << algorithm_properties["TRTIS_SERVER"] << endl;
+    cout << "TRTIS server name and port = " << algorithm_properties["TRTIS_SERVER"] << endl;
 
-    if (argc > 3) { algorithm_properties["CONFIDENCE_THRESHOLD"] = string(argv[3]); }
-    cout << "Confidence threshold = " << algorithm_properties["CONFIDENCE_THRESHOLD"] << endl;
+    if (argc > 3) { algorithm_properties["CLASS_CONFIDENCE_THRESHOLD"] = string(argv[3]); }
+    cout << "Class confidence threshold = " << algorithm_properties["CLASS_CONFIDENCE_THRESHOLD"] << endl;
+
+    if (argc > 4) { algorithm_properties["EXTRA_CONFIDENCE_THRESHOLD"] = string(argv[4]); }
+    cout << "Extra confidence threshold = " << algorithm_properties["EXTRA_CONFIDENCE_THRESHOLD"] << endl;
 
     Properties media_properties;
     string job_name("Testing TRTIS");
 
     MPFImageLocationVec detections;
     MPFImageJob job(job_name, uri, algorithm_properties, media_properties);
+
+    cout << "Running job..." << endl;
     detections = trtis_component.GetDetections(job);
 
-    cout << "Found " << detections.size() << " detections." << endl;
+    cout << "\nFound " << detections.size() << " detections." << endl;
 
     for (int i = 0; i < detections.size(); i++) {
         string feature_type = detections[i].detection_properties["FEATURE TYPE"];
-        cout << "Detection number " << i << ":\n"
+        cout << "\nDetection number " << i << ":\n"
              << "\tFEATURE TYPE = " << feature_type << endl;
         if (feature_type == "CLASS") {
             cout << "\tCLASSIFICATION = " << detections[i].detection_properties["CLASSIFICATION"] << endl;
@@ -87,5 +102,10 @@ int main(int argc, char *argv[]) {
     }
 
     trtis_component.Close();
+
+    if (!trtis_component.Close()) {
+        cout << "Component teardown failed." << endl;
+    }
+
     return EXIT_SUCCESS;
 }
