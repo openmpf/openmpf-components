676bcc3eeecd5ef931b9748c7ef8cba1fefa35e8
Jeff Robble <jrobble@mitre.org>
2020-11-17 11:45:06 -0500

    Update versions to 5.1. (#205)

--- python/AzureOcrTextDetection/setup.py
+++ python/AzureOcrTextDetection/setup.py
@@ -29,11 +29,11 @@ import setuptools
 
 setuptools.setup(
     name='AzureOcrTextDetection',
-    version='5.0',
+    version='5.1',
     packages=setuptools.find_packages(exclude=('*test*',)),
     install_requires=(
-        'mpf_component_api>=5.0',
-        'mpf_component_util>=5.0',
+        'mpf_component_api>=5.1',
+        'mpf_component_util>=5.1',
         'opencv-python>=3.3',
         'numpy>=1.11'
     ),
