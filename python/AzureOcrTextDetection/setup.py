46940054bb81e3bc1801aabe0f5a0881650fe986
Jeff Robble <jrobble@mitre.org>
2020-12-18 14:58:31 -0500

    Update versions to 6.0. (#215)

--- python/AzureOcrTextDetection/setup.py
+++ python/AzureOcrTextDetection/setup.py
@@ -29,11 +29,11 @@ import setuptools
 
 setuptools.setup(
     name='AzureOcrTextDetection',
-    version='5.1',
+    version='6.0',
     packages=setuptools.find_packages(exclude=('*test*',)),
     install_requires=(
-        'mpf_component_api>=5.1',
-        'mpf_component_util>=5.1',
+        'mpf_component_api>=6.0',
+        'mpf_component_util>=6.0',
         'opencv-python>=4.4.0',
         'numpy>=1.11'
     ),
--- python/EastTextDetection/setup.py
+++ python/EastTextDetection/setup.py
@@ -29,12 +29,12 @@ import setuptools
 
 setuptools.setup(
     name='EastTextDetection',
-    version='5.1',
+    version='6.0',
     packages=setuptools.find_packages(exclude=('*test*',)),
     package_data={'': ['east_resnet50.pb']},
     install_requires=(
-        'mpf_component_api>=5.1',
-        'mpf_component_util>=5.1',
+        'mpf_component_api>=6.0',
+        'mpf_component_util>=6.0',
         'opencv-python>=4.4.0',
     ),
     entry_points={
