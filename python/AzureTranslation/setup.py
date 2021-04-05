525d897643a3547762de6ee07ab291131fd69b80
Jeff Robble <jrobble@mitre.org>
2021-04-05 10:11:08 -0400

    Update versions to 6.1. (#242)

--- python/AzureTranslation/setup.py
+++ python/AzureTranslation/setup.py
@@ -29,12 +29,12 @@ import setuptools
 
 setuptools.setup(
     name='AzureTranslation',
-    version='6.0',
+    version='6.1',
     packages=setuptools.find_packages(exclude=('*test*',)),
     package_data={'': ['text-tags.json']},
     install_requires=(
-        'mpf_component_api>=6.0',
-        'mpf_component_util>=6.0',
+        'mpf_component_api>=6.1',
+        'mpf_component_util>=6.1',
     ),
     entry_points={
         'mpf.exported_component': 'component = acs_translation_component:AcsTranslationComponent'
