f8a8c0f0d926dcecc9d590884c46ffa1a832de70
Howard Huang <hhuang@mitre.org>
2021-07-28 14:33:01 -0400

    Update Tike PDFParser to track embedded object ID and extension.

--- java/TikaImageDetection/src/main/java/org/apache/tika/parser/pdf/ImageGraphicsEngine.java
+++ java/TikaImageDetection/src/main/java/org/apache/tika/parser/pdf/ImageGraphicsEngine.java
@@ -1,19 +1,29 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/******************************************************************************
+ * NOTICE                                                                     *
+ *                                                                            *
+ * This software (or technical data) was produced for the U.S. Government     *
+ * under contract, and is subject to the Rights in Data-General Clause        *
+ * 52.227-14, Alt. IV (DEC 2007).                                             *
+ *                                                                            *
+ * Copyright 2021 The MITRE Corporation. All Rights Reserved.                 *
+ ******************************************************************************/
+
+/******************************************************************************
+ * Copyright 2021 The MITRE Corporation                                       *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ *    http://www.apache.org/licenses/LICENSE-2.0                              *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ ******************************************************************************/
+
 package org.apache.tika.parser.pdf;
 
 import org.apache.pdfbox.contentstream.PDFGraphicsStreamEngine;
@@ -372,6 +382,13 @@ class ImageGraphicsEngine extends PDFGraphicsStreamEngine {
             //TODO: determine if we need to add more image types
 //                    throw new RuntimeException("EXTEN:" + extension);
         }
+
+        // MPF edit: return exact image extension and store embedded object ID in Metadata.
+        PDImageXObject xobject = (PDImageXObject) pdImage;
+        COSStream cosStream = xobject.getCOSObject();
+        metadata.set(Metadata.EMBEDDED_RELATIONSHIP_ID, cosStream.toString());
+        metadata.set(Metadata.CONTENT_TYPE, suffix);
+
         if (hasMasks(pdImage)) {
             // TIKA-3040, PDFBOX-4771: can't save ARGB as JPEG
             suffix = "png";
