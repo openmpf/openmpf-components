8aaa3bdc3c237d9c0aeb5cea71868fd78992b44b
jrobble <jrobble@mitre.org>
2025-03-13 04:56:14 +0000

    Implemented unit tests.

--- python/LlamaVideoSummarization/tests/test_llama_video_summarization.py
+++ python/LlamaVideoSummarization/tests/test_llama_video_summarization.py
@@ -27,30 +27,54 @@
 from __future__ import annotations
 
 import logging
+import os
 import pathlib
 import sys
 import unittest
+import unittest.mock
 
 import mpf_component_api as mpf
 
 sys.path.insert(0, str(pathlib.Path(__file__).parent.parent))
-from llama_video_summarization_component import LlamaVideoSummarizationComponent
+from llama_video_summarization_component import LlamaVideoSummarizationComponent, ChildProcess
 
 logging.basicConfig(level=logging.DEBUG)
 
+USE_MOCKS = eval(os.environ.get('USE_MOCKS', 'True'))
 TEST_DATA = pathlib.Path(__file__).parent / 'data'
 
 class TestComponent(unittest.TestCase):
+
+    def run_patched_job(self, component, job, response):
+        detection_func = component.get_detections_from_video
+
+        if not USE_MOCKS:
+            return detection_func(job)
+
+        with unittest.mock.patch.object(ChildProcess, '__init__', lambda: None):
+            with unittest.mock.patch.object(ChildProcess, '__del__', lambda: None):
+                with unittest.mock.patch.object(ChildProcess, 'send_job_get_response', lambda x, y: response):
+                    return detection_func(job)
 
     def test_video(self):
         component = LlamaVideoSummarizationComponent()
 
         job = mpf.VideoJob('cat job', str(TEST_DATA / 'cat.mp4'), 0, 100, {}, {})
-        results = component.get_detections_from_video(job)
+        results = self.run_patched_job(component, job,
+            {
+                "video_summary": "This is a video of a cat.",
+                "video_length": 100,
+                "video_event_timeline": "{json}"
+            })
         self.assertIn("cat", results[0].detection_properties["TEXT"])
 
-        job = mpf.VideoJob('dog job', str(TEST_DATA / 'dog.mp4'), 0, 200, {}, {})
-        results = component.get_detections_from_video(job)
+        job = mpf.VideoJob('dog job', str(TEST_DATA / 'dog.mp4'), 0, 100, {}, {})
+        results = self.run_patched_job(component, job,
+            {
+                "video_summary": "This is a video of a dog.",
+                "video_length": 200,
+                "video_event_timeline": "{json}"
+            })
         self.assertIn("dog", results[0].detection_properties["TEXT"])
 
 if __name__ == "__main__":
