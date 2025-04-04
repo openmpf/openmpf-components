7f929ecc7b47d0da30b5a35e4d41cc257f079b5b
ZachCafego <zacharycafego@gmail.com>
2025-04-04 13:19:05 -0400

    Start of Pixtral component.

---
+++ python/PixtralDetection/pixtral_component/pixtral_component.py
@@ -0,0 +1,253 @@
+#############################################################################
+# NOTICE                                                                    #
+#                                                                           #
+# This software (or technical data) was produced for the U.S. Government    #
+# under contract, and is subject to the Rights in Data-General Clause       #
+# 52.227-14, Alt. IV (DEC 2007).                                            #
+#                                                                           #
+# Copyright 2024 The MITRE Corporation. All Rights Reserved.                #
+#############################################################################
+
+#############################################################################
+# Copyright 2024 The MITRE Corporation                                      #
+#                                                                           #
+# Licensed under the Apache License, Version 2.0 (the "License");           #
+# you may not use this file except in compliance with the License.          #
+# You may obtain a copy of the License at                                   #
+#                                                                           #
+#    http://www.apache.org/licenses/LICENSE-2.0                             #
+#                                                                           #
+# Unless required by applicable law or agreed to in writing, software       #
+# distributed under the License is distributed on an "AS IS" BASIS,         #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  #
+# See the License for the specific language governing permissions and       #
+# limitations under the License.                                            #
+#############################################################################
+
+import time
+import os
+import cv2
+import base64
+import json
+import re
+import math
+from vllm import LLM
+from vllm.sampling_params import SamplingParams
+
+import logging
+from typing import Mapping, Iterable
+
+import mpf_component_api as mpf
+import mpf_component_util as mpf_util
+
+logger = logging.getLogger('PixtralComponent')
+
+class PixtralComponent:
+    detection_type = 'CLASS'
+
+    def __init__(self):
+        self.model_name = 'mistralai/Pixtral-12B-2409'
+        self.host_url = ''
+        self.sampling_params = SamplingParams(max_tokens=8192)
+        self.llm = LLM(
+            model=self.model_name,
+            tokenizer_mode="mistral",
+            max_model_len=4096,
+            max_num_seqs=2
+        )
+        self.class_prompts = dict()
+        self.json_class_prompts = dict()
+        self.frame_prompts = dict()
+        self.json_frame_prompts = dict()
+
+        self.json_limit = 3
+
+    def get_detections_from_image(self, image_job: mpf.ImageJob) -> Iterable[mpf.ImageLocation]:
+        logger.info('Received image job: %s', image_job.job_name)
+
+        config = JobConfig(image_job.job_properties)
+        image_reader = mpf_util.ImageReader(image_job)
+
+        if image_job.feed_forward_location is None:
+            detections = self._get_frame_detections(image_job, [image_reader.get_image(),], config)
+        else:
+            raise mpf.DetectionException()
+        
+        logger.info(f"Job complete. Found {len(detections)} detections.")
+        return detections
+
+    def _get_frame_detections(self, job, reader, config):
+        self._update_prompts(config.prompt_config_path, config.json_prompt_config_path)
+
+        tracks = []
+        for idx, frame in enumerate(reader):
+            self.frame_count += 1
+
+            height, width, _ = frame.shape
+            detection_properties = dict()
+
+            encoded = self._encode_image(frame, job.media_properties['MIME_TYPE'])
+            for tag, prompt in self.json_frame_prompts:
+                json_attempts, json_failed = 0, True
+                while (json_attempts <= self.json_limit) and (json_failed):  
+                    json_attempts += 1
+                    response = self._get_pixtral_response(prompt, encoded)
+                    try:
+                        response = response.split('```json\n')[1].split('```')[0]
+                        response_json = json.loads(response)
+                        self._update_detection_properties(detection_properties, response_json)
+                        json_failed = False
+                    except:
+                        logger.warning(f"Pixtral failed to produce valid JSON output. Failed {json_attempts} of {self.json_limit} attempts.")
+                        continue
+
+            img_location = mpf.ImageLocation(0, 0, width, height, -1, detection_properties)
+            tracks.append(img_location)
+
+        return tracks
+
+    def _update_detection_properties(self, detection_properties, response_json):
+        ignore_words = ['unsure', 'none', 'false', 'no', 'unclear', '']
+        key_list = self._get_keys(response_json)
+        key_vals = dict()
+        keywords = []
+        for key_str in key_list:
+            split_key = [' '.join(x.split('_')) for x in ('pixtral' + key_str).split('||')]
+            key, val = " ".join([s.upper() for s in split_key[:-1]]), split_key[-1]
+            key_vals[key] = val
+
+        if ('PIXTRAL VISIBLE PERSON' not in key_vals) or (key_vals['PIXTRAL VISIBLE PERSON'].strip().lower() not in ignore_words):
+            for key, val in key_vals.items():
+                if ('VISIBLE' in key) and (val.strip().lower() in ignore_words):
+                    keywords.append(key.split(' VISIBLE ')[1])
+                    key_vals.pop(key)
+
+            for keyword in keywords:
+                pattern = re.compile(fr'\b{keyword}\b')
+                for key_to_remove in filter(pattern.search, key_vals):
+                    key_vals.pop(key_to_remove)
+            
+            for key, val in key_vals.items():
+                if val.strip().lower() in ignore_words:
+                    key_vals.pop(key)
+            
+            detection_properties.update(key_vals)
+            detection_properties['ANNOTATED BY PIXTRAL'] = True
+
+    def _get_keys(self, response_json):
+        if isinstance(response_json, list) or isinstance(response_json, str):
+            yield f'||{json.dumps(response_json)}'
+        elif isinstance(response_json, dict):
+            if self._is_lowest_level(response_json):
+                yield f'||{json.dumps(response_json)}'
+            else:
+                for key, value in response_json.items():
+                    yield from (f'||{key}{p}' for p in self._get_keys(value))
+        
+    @staticmethod
+    def _is_lowest_level(response_json):
+        for key, val in response_json.items():
+            if not isinstance(val, str):
+                return False
+        return True
+
+    def _encode_image(self, image, mime_type):
+        encode_params = [int(cv2.IMWRITE_PNG_COMPRESSION), 9]
+        _, buffer = cv2.imencode('.png', image, encode_params)
+        encoded_string = base64.b64encode(buffer).decode("utf-8")
+        
+        return f"data:{mime_type};base64,{encoded_string}"
+
+    def _update_prompts(self, prompt_config_path, json_prompt_config_path):
+        '''
+        Updates self.class_prompts dictionary to have the following format
+
+        {
+            CLASS1: {TAG1: PROMPT1},
+            CLASS2: {TAG2: PROMPT2, TAG3: PROMPT3},
+            ...
+        }
+
+        and self.frame_prompts to be a dict of key, prompt string pairs.
+        '''
+        try:
+            with open(prompt_config_path, 'r') as f:
+                data = json.load(f)
+                class_dicts, frame_dicts = data['classPrompts'], data['framePrompts']
+                for class_dict in class_dicts:
+                    classes, prompts = [cls.lower() for cls in class_dict['classes']], class_dict['prompts']
+                    for cls in classes:
+                        if cls not in self.class_prompts:
+                            self.class_prompts[cls] = dict()
+                        self.class_prompts[cls].update({ dct['detectionProperty']:dct['prompt'] for dct in prompts })
+
+                for frame_dict in frame_dicts:
+                    self.frame_prompts[frame_dict['detectionProperty']] = frame_dict['prompt']
+            
+            with open(json_prompt_config_path, 'r') as f:
+                data = json.load(f)
+                json_class_dicts, json_frame_dicts = data['classPrompts'], data['framePrompts']
+                for class_dict in json_class_dicts:
+                    classes, prompts = [cls.lower() for cls in class_dict['classes']], class_dict['prompts']
+                    for cls in classes:
+                        for idx, prompt in enumerate(prompts):
+                            self.json_class_prompts[cls] = { f'JSON_{idx}':prompt }
+                
+                for idx, frame_dict in enumerate(json_frame_dicts):
+                    self.json_frame_prompts[f'JSON_{idx}'] = frame_dict['prompt']
+ 
+        except Exception as e:
+            raise mpf.DetectionException(
+                f"Invalid JSON structure for component: {e}",
+                mpf.DetectionError.COULD_NOT_READ_DATAFILE
+            )
+
+    def _get_pixtral_response(self, prompt, encoded_image):
+        try:
+            messages = [{"role": "user", "content": [{"type": "text", "text": prompt}, {"type": "image_url", "image_url": {"url": encoded_image}}]}]
+            outputs = self.llm.chat(messages, sampling_params=self.sampling_params)
+            return outputs[0].outputs[0].text
+        except:
+            raise mpf.DetectionException(
+                "Could not communicate with Pixtral server: ",
+                mpf.DetectionError.NETWORK_ERROR
+            )
+    
+class JobConfig:
+    def __init__(self, job_properties: Mapping[str, str], media_properties=None):
+        self.prompt_config_path = self._get_prop(job_properties, "PROMPT_CONFIGURATION_PATH", "")
+        if self.prompt_config_path == "":
+            self.prompt_config_path = os.path.join(os.path.dirname(__file__), 'data', 'crypto_prompts.json')
+        
+        self.json_prompt_config_path = self._get_prop(job_properties, "JSON_PROMPT_CONFIGURATION_PATH", "")
+        if self.json_prompt_config_path == "":
+            self.json_prompt_config_path = os.path.join(os.path.dirname(__file__), 'data', 'crypto_json_prompts.json')
+	
+    @staticmethod
+    def _get_prop(job_properties, key, default_value, accept_values=[]):
+        prop = mpf_util.get_property(job_properties, key, default_value)
+        if (accept_values != []) and (prop not in accept_values):
+            raise mpf.DetectionException(
+                f"Property {key} not in list of acceptable values: {accept_values}",
+                mpf.DetectionError.INVALID_PROPERTY
+            )
+        return prop
+
+class Timer:
+    def __init__(self):
+        self._seconds_elapsed = 0.0
+        self._last_start_time = None
+
+    def start(self):
+        if self._last_start_time is None:
+            self._last_start_time = time.perf_counter()
+
+    def pause(self):
+        if self._last_start_time is not None:
+            self._seconds_elapsed += time.perf_counter() - self._last_start_time
+            self._last_start_time = None
+
+    def get_seconds_elapsed_from_last_pause(self) -> float:
+        return self._seconds_elapsed
+
+EXPORT_MPF_COMPONENT = PixtralComponent
---
+++ python/PixtralDetection/tests/test_pixtral.py
@@ -0,0 +1,61 @@
+#############################################################################
+# NOTICE                                                                    #
+#                                                                           #
+# This software (or technical data) was produced for the U.S. Government    #
+# under contract, and is subject to the Rights in Data-General Clause       #
+# 52.227-14, Alt. IV (DEC 2007).                                            #
+#                                                                           #
+# Copyright 2024 The MITRE Corporation. All Rights Reserved.                #
+#############################################################################
+
+#############################################################################
+# Copyright 2024 The MITRE Corporation                                      #
+#                                                                           #
+# Licensed under the Apache License, Version 2.0 (the "License");           #
+# you may not use this file except in compliance with the License.          #
+# You may obtain a copy of the License at                                   #
+#                                                                           #
+#    http://www.apache.org/licenses/LICENSE-2.0                             #
+#                                                                           #
+# Unless required by applicable law or agreed to in writing, software       #
+# distributed under the License is distributed on an "AS IS" BASIS,         #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  #
+# See the License for the specific language governing permissions and       #
+# limitations under the License.                                            #
+#############################################################################
+
+import sys
+import os
+import logging
+
+# Add pixtral_component to path.
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from pixtral_component.pixtral_component import PixtralComponent
+
+import unittest
+import mpf_component_api as mpf
+
+logging.basicConfig(level=logging.DEBUG)
+
+class TestPixtral(unittest.TestCase):
+    def test_image_file(self):
+        job = mpf.ImageJob(
+            job_name='test-image',
+            data_uri=self._get_test_file('bitcoin.jpeg'),
+            job_properties=dict(),
+            media_properties={
+                'MIME_TYPE': 'image/jpeg'
+            },
+            feed_forward_location=None
+        )
+        component = PixtralComponent()
+
+        result = component.get_detections_from_image(job)[0]
+        print(result)
+
+    @staticmethod
+    def _get_test_file(filename):
+        return os.path.join(os.path.dirname(__file__), 'data', filename)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
