605932e6beae8e4c622c90c5eb1eff7654423496
ZachCafego <zacharycafego@gmail.com>
2025-04-08 15:10:29 +0000

    Added support for custom model temperature.

--- python/PixtralDetection/pixtral_component/pixtral_component.py
+++ python/PixtralDetection/pixtral_component/pixtral_component.py
@@ -30,7 +30,6 @@ import cv2
 import base64
 import json
 import re
-import math
 from vllm import LLM
 from vllm.sampling_params import SamplingParams
 
@@ -42,18 +41,23 @@ import mpf_component_util as mpf_util
 
 logger = logging.getLogger('PixtralComponent')
 
+os.environ['HF_HUB_OFFLINE'] = '1'
+
+IGNORE_WORDS = ('unsure', 'none', 'false', 'no', 'unclear', 'n/a', 'unspecified', 'unknown', 'unreadable', 'not visible', 'none visible')
+IGNORE_PREFIXES = tuple([s + ' ' for s in IGNORE_WORDS])
+
 class PixtralComponent:
     detection_type = 'CLASS'
 
     def __init__(self):
-        self.model_name = 'mistralai/Pixtral-12B-2409'
+        # self.model_name = 'mistralai/Pixtral-12B-2409'
+        self.model_name = '/root/.cache/huggingface/hub/models--mistralai--Pixtral-12B-2409/snapshots/c21b6fd59bfe3b1246861d2811d0d6ae53f78915'
         self.host_url = ''
-        self.sampling_params = SamplingParams(max_tokens=8192)
+        self.sampling_params = None
         self.llm = LLM(
             model=self.model_name,
-            tokenizer_mode="mistral",
-            max_model_len=4096,
-            max_num_seqs=2
+            tokenizer_mode="mistral"
+            # max_model_len=13472
         )
         self.class_prompts = dict()
         self.json_class_prompts = dict()
@@ -68,10 +72,15 @@ class PixtralComponent:
         config = JobConfig(image_job.job_properties)
         image_reader = mpf_util.ImageReader(image_job)
 
+        self.sampling_params = SamplingParams(max_tokens=8192, temperature=config.temperature)
+
         if image_job.feed_forward_location is None:
             detections = self._get_frame_detections(image_job, [image_reader.get_image(),], config)
         else:
-            raise mpf.DetectionException()
+            raise mpf.DetectionException(
+                "Feed forward jobs not supported yet: ",
+                mpf.DetectionError.DETECTION_FAILED
+            )
         
         logger.info(f"Job complete. Found {len(detections)} detections.")
         return detections
@@ -81,25 +90,31 @@ class PixtralComponent:
 
         tracks = []
         for idx, frame in enumerate(reader):
-            self.frame_count += 1
-
             height, width, _ = frame.shape
-            detection_properties = dict()
+            detection_properties = { 'CLASSIFICATION': 'SCREENSHOT' }
 
             encoded = self._encode_image(frame, job.media_properties['MIME_TYPE'])
-            for tag, prompt in self.json_frame_prompts:
+            for tag, prompt in self.json_frame_prompts.items():
                 json_attempts, json_failed = 0, True
-                while (json_attempts <= self.json_limit) and (json_failed):  
+                while (json_attempts < self.json_limit) and (json_failed):  
                     json_attempts += 1
                     response = self._get_pixtral_response(prompt, encoded)
                     try:
-                        response = response.split('```json\n')[1].split('```')[0]
+                        logger.info(response)
+                        if response.startswith('```json\n'):
+                            response = response.split('```json\n')[1].split('```')[0]
                         response_json = json.loads(response)
                         self._update_detection_properties(detection_properties, response_json)
                         json_failed = False
-                    except:
-                        logger.warning(f"Pixtral failed to produce valid JSON output. Failed {json_attempts} of {self.json_limit} attempts.")
+                    except Exception as e:
+                        logger.warning(f"Pixtral failed to produce valid JSON output: {e}.")
+                        logger.warning(f"Failed {json_attempts} of {self.json_limit} attempts.")
                         continue
+
+                if json_failed:
+                    logger.warning("Using last full Pixtral response instead of parsed JSON output.")
+                    detection_properties["FAILED TO PROCESS PIXTRAL RESPONSE"] = True
+                    detection_properties["FULL PIXTRAL RESPONSE"] = response
 
             img_location = mpf.ImageLocation(0, 0, width, height, -1, detection_properties)
             tracks.append(img_location)
@@ -107,8 +122,7 @@ class PixtralComponent:
         return tracks
 
     def _update_detection_properties(self, detection_properties, response_json):
-        ignore_words = ['unsure', 'none', 'false', 'no', 'unclear', '']
-        key_list = self._get_keys(response_json)
+        key_list = self._get_keys(response_json) 
         key_vals = dict()
         keywords = []
         for key_str in key_list:
@@ -116,40 +130,74 @@ class PixtralComponent:
             key, val = " ".join([s.upper() for s in split_key[:-1]]), split_key[-1]
             key_vals[key] = val
 
-        if ('PIXTRAL VISIBLE PERSON' not in key_vals) or (key_vals['PIXTRAL VISIBLE PERSON'].strip().lower() not in ignore_words):
-            for key, val in key_vals.items():
-                if ('VISIBLE' in key) and (val.strip().lower() in ignore_words):
+        tmp_key_vals = dict(key_vals)
+        for key, val in key_vals.items():
+            if 'VISIBLE' in key:
+                tmp_key_vals.pop(key)
+                if self._ignore(val):
                     keywords.append(key.split(' VISIBLE ')[1])
-                    key_vals.pop(key)
+        key_vals = tmp_key_vals
 
-            for keyword in keywords:
-                pattern = re.compile(fr'\b{keyword}\b')
-                for key_to_remove in filter(pattern.search, key_vals):
-                    key_vals.pop(key_to_remove)
-            
-            for key, val in key_vals.items():
-                if val.strip().lower() in ignore_words:
-                    key_vals.pop(key)
-            
-            detection_properties.update(key_vals)
-            detection_properties['ANNOTATED BY PIXTRAL'] = True
+        tmp_key_vals = dict(key_vals)
+        for keyword in keywords:
+            pattern = re.compile(fr'\b{keyword}\b')
+            for key_to_remove in filter(pattern.search, key_vals):
+                tmp_key_vals.pop(key_to_remove, None)
+        key_vals = tmp_key_vals
+        
+        tmp_key_vals = dict(key_vals)
+        for key, val in key_vals.items():
+            if self._ignore(val):
+                tmp_key_vals.pop(key)
+        key_vals = tmp_key_vals
+        
+        detection_properties.update(key_vals)
+        detection_properties['ANNOTATED BY PIXTRAL'] = True
 
     def _get_keys(self, response_json):
-        if isinstance(response_json, list) or isinstance(response_json, str):
-            yield f'||{json.dumps(response_json)}'
+        if not response_json:
+            yield f'||none'
+
+        elif isinstance(response_json, (str, bool)):
+            yield f'||{response_json}'
+
+        elif isinstance(response_json, list):
+            for elt in response_json:
+                yield from (f'||{key}' for key in self._get_keys(elt))
+
         elif isinstance(response_json, dict):
             if self._is_lowest_level(response_json):
-                yield f'||{json.dumps(response_json)}'
+
+                tmp_response_json = dict(response_json)
+                for key, val in response_json.items():
+                    if self._ignore(val):
+                        tmp_response_json.pop(key)
+                response_json = tmp_response_json
+
+                if not response_json:
+                    yield f'||none'
+                else:
+                    yield from (f'||{key}||{val}' for key, val in response_json.items())
+
             else:
                 for key, value in response_json.items():
-                    yield from (f'||{key}{p}' for p in self._get_keys(value))
+                    if self._ignore(key):
+                        yield f'||none'
+                    else:
+                        yield from (f'||{key}{p}' for p in self._get_keys(value))
         
     @staticmethod
     def _is_lowest_level(response_json):
         for key, val in response_json.items():
-            if not isinstance(val, str):
+            if not isinstance(val, (str, bool)):
                 return False
         return True
+    
+    @staticmethod
+    def _ignore(input):
+        return not input or \
+            input.strip().lower() in IGNORE_WORDS or \
+            input.strip().lower().startswith(IGNORE_PREFIXES)
 
     def _encode_image(self, image, mime_type):
         encode_params = [int(cv2.IMWRITE_PNG_COMPRESSION), 9]
@@ -214,7 +262,7 @@ class PixtralComponent:
             )
     
 class JobConfig:
-    def __init__(self, job_properties: Mapping[str, str], media_properties=None):
+    def __init__(self, job_properties: Mapping[str, str]):
         self.prompt_config_path = self._get_prop(job_properties, "PROMPT_CONFIGURATION_PATH", "")
         if self.prompt_config_path == "":
             self.prompt_config_path = os.path.join(os.path.dirname(__file__), 'data', 'crypto_prompts.json')
@@ -222,6 +270,13 @@ class JobConfig:
         self.json_prompt_config_path = self._get_prop(job_properties, "JSON_PROMPT_CONFIGURATION_PATH", "")
         if self.json_prompt_config_path == "":
             self.json_prompt_config_path = os.path.join(os.path.dirname(__file__), 'data', 'crypto_json_prompts.json')
+        
+        self.temperature = self._get_prop(job_properties, "TEMPERATURE", 0.7)
+        if self.temperature > 1.0 or self.temperature < 0.0:
+            raise mpf.DetectionException(
+                "Invalid value for TEMPERATURE. Make sure it is set to a value between 0 and 1, inclusive.",
+                mpf.DetectionError.INVALID_PROPERTY
+            )
 	
     @staticmethod
     def _get_prop(job_properties, key, default_value, accept_values=[]):
