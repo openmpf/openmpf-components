198f3ecb7815e1d659ee7936dd390e9e09a40269
Eric McCann <emccann@mitre.org>
2025-12-16 15:56:01 +0000

    Include served-model-name param in the entrypoint, not the CMD

--- python/QwenSpeechSummarization/vllm-entrypoint.sh
+++ python/QwenSpeechSummarization/vllm-entrypoint.sh
@@ -6,7 +6,7 @@ model_string="$(echo "${VLLM_MODEL}" | sed 's/\//--/g')" # replace / with --
 snapshot_glob="/root/.cache/huggingface/hub/models--${model_string}/snapshots/*/"
 
 for x in $snapshot_glob; do
-    vllm serve $x "$@" || continue
+    vllm serve $x --served-model-name "${VLLM_MODEL}" "$@" || continue
     exit 0
 done
 echo "Failed to find a valid snapshot directory for the model" 1>&2
