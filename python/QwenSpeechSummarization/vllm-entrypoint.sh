84e170c76d6c190bab5b6663fbce9f0e2263a9dd
Eric McCann <emccann@mitre.org>
2026-01-15 14:33:40 +0000

    Fix max-model-length parameter name

--- python/QwenSpeechSummarization/vllm-entrypoint.sh
+++ python/QwenSpeechSummarization/vllm-entrypoint.sh
@@ -32,7 +32,7 @@ model_string="$(echo "${VLLM_MODEL}" | sed 's/\//--/g')" # replace / with --
 snapshot_glob="/root/.cache/huggingface/hub/models--${model_string}/snapshots/*/"
 
 for x in $snapshot_glob; do
-    vllm serve $x --served-model-name "${VLLM_MODEL}" --model-max-len ${MODEL_MAX_LEN} "$@" || continue
+    vllm serve $x --served-model-name "${VLLM_MODEL}" --max-model-len ${MAX_MODEL_LEN} "$@" || continue
     exit 0
 done
 echo "Failed to find a valid snapshot directory for the model" 1>&2
