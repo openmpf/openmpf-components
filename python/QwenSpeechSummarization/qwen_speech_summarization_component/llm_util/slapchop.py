39810e49f0154662ff2e95ef0b6f7431e675c4e5
Eric McCann <emccann@mitre.org>
2026-01-30 18:27:51 +0000

    Force minimum_chunk_size=2 by default to force json array chunking to either make progress or error RATHER THAN possibly iterating forever in summarize_summaries if only a single summary can be summarized in one context

--- python/QwenSpeechSummarization/qwen_speech_summarization_component/llm_util/slapchop.py
+++ python/QwenSpeechSummarization/qwen_speech_summarization_component/llm_util/slapchop.py
@@ -88,7 +88,7 @@ def split_csv_into_chunks(tokenizer, text: str, chunk_size: int = 10000, overlap
 
     return _chunk_within_limits(total_rows, chunk_size, overlap, token_count_at_line, min_grouping, lambda i: df.iloc[i], convert_chunk_to_csv) # type: ignore
 
-def split_array_into_chunks(tokenizer, arr: List[Any], chunk_size: int = 10000, overlap: int = 500, min_grouping=-1):
+def split_array_into_chunks(tokenizer, arr: List[Any], chunk_size: int = 10000, overlap: int = 500, min_grouping=2):
     for i in range(0, len(arr)):
         if type(arr[i]) is not str:
             arr[i] = arr[i].json() if hasattr(arr[i], 'json') else json.dumps(arr[i])
