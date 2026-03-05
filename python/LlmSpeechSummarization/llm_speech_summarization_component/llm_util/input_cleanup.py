91987cf839983b8b0ab3c98f7c081ba496c4ec0e
Eric McCann <emccann@mitre.org>
2026-03-05 15:39:29 +0000

    Ensure text is always a string before trying to remove newlines

--- python/LlmSpeechSummarization/llm_speech_summarization_component/llm_util/input_cleanup.py
+++ python/LlmSpeechSummarization/llm_speech_summarization_component/llm_util/input_cleanup.py
@@ -34,7 +34,7 @@ def convert_speech_tracks_to_csv(input: List[mpf.VideoTrack]|List[mpf.AudioTrack
     writer = DictWriter(buffer, ['speaker_id', 'gender', 'start_timestamp', 'end_timestamp', 'english_text', 'original_language'], delimiter='|')
     writer.writeheader()
     for track in input:
-        text = track.detection_properties['TRANSLATION'] if 'TRANSLATION' in track.detection_properties else track.detection_properties['TRANSCRIPT']
+        text = str(track.detection_properties['TRANSLATION'] if 'TRANSLATION' in track.detection_properties else track.detection_properties['TRANSCRIPT'])
         # this is a slight compromise BUT spoken newlines don't exist. If it's one utterance, treat as one line.
         text.replace('\n', ' ')
         writer.writerow({
